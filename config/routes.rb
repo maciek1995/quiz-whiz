70a4d0d15e1adee5780751ee850b800faee42873
johnny <jan.piotr.latko@gmail.com>
2016-12-12 20:20:47 +0100

    messages channel

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+
+  mount ActionCable.server => '/cable'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
@@ -7,4 +10,6 @@ Rails.application.routes.draw do
     get :current
     get :play_now
   end
+
+  resources :messages, only: [:create]
 end
