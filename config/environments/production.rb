0df413a34dd2e8bb421af13aa88ba911167928f7
Wojciech Pospieszynski <pospieszynski.wojciech@gmail.com>
2017-01-18 00:39:52 +0100

    changed websocket config on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.url = "ws://intense-garden-49310.herokuapp.com/cable"
+  
 end
